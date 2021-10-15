b25cd443341bfb12c02c5980328d29d9a35b8328
Colinh36 <colinh647@gmail.com>
2021-10-15 19:10:44 +0800

    feature/fix all warning

--- src/components/ContactUs/index.js
+++ src/components/ContactUs/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import { contactUs } from "../../redux/actions/formsActions";
 import { getBase64 } from "../../utils/base64";
