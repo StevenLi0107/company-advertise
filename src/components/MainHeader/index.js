7aed3e556b5175cb61e635af66d3385a9f73cc7f
Colinh36 <colinh647@gmail.com>
2021-10-19 17:20:07 +0800

    feature/fix mail to issue

--- src/components/MainHeader/index.js
+++ src/components/MainHeader/index.js
@@ -29,7 +29,7 @@ const MainHeader = ({ scrollToElement }) => {
           <div className="container SubHeader-ContainerContent-module--cls2--3QP34 SubHeader-ContainerContent-module--cls1--3p7SF">
             <div className="SubHeader-LeftBlock-module--cls2--76WXv SubHeader-LeftBlock-module--cls1--2hkJC">
               <a
-                href="mailto:contact@cowork.dev"
+                href="mailto:mail.cowork.dev@gmail.com"
                 className="SubHeader-Link-module--cls2--goBAf SubHeader-Link-module--cls1--5MFMz"
               >
                 <svg
@@ -64,10 +64,7 @@ const MainHeader = ({ scrollToElement }) => {
                 >
                   <path
                     fill="#ffffff"
-                    d="M72.089,0.02L59.624,0C45.62,0,36.57,9.285,36.57,23.656v10.907H24.037c-1.083,0-1.96,0.878-1.96,1.961v15.803
- c0,1.083,0.878,1.96,1.96,1.96h12.533v39.876c0,1.083,0.877,1.96,1.96,1.96h16.352c1.083,0,1.96-0.878,1.96-1.96V54.287h14.654
- c1.083,0,1.96-0.877,1.96-1.96l0.006-15.803c0-0.52-0.207-1.018-0.574-1.386c-0.367-0.368-0.867-0.575-1.387-0.575H56.842v-9.246
- c0-4.444,1.059-6.7,6.848-6.7l8.397-0.003c1.082,0,1.959-0.878,1.959-1.96V1.98C74.046,0.899,73.17,0.022,72.089,0.02z"
+                    d="M72.089,0.02L59.624,0C45.62,0,36.57,9.285,36.57,23.656v10.907H24.037c-1.083,0-1.96,0.878-1.96,1.961v15.803  c0,1.083,0.878,1.96,1.96,1.96h12.533v39.876c0,1.083,0.877,1.96,1.96,1.96h16.352c1.083,0,1.96-0.878,1.96-1.96V54.287h14.654  c1.083,0,1.96-0.877,1.96-1.96l0.006-15.803c0-0.52-0.207-1.018-0.574-1.386c-0.367-0.368-0.867-0.575-1.387-0.575H56.842v-9.246  c0-4.444,1.059-6.7,6.848-6.7l8.397-0.003c1.082,0,1.959-0.878,1.959-1.96V1.98C74.046,0.899,73.17,0.022,72.089,0.02z"
                   ></path>
                 </svg>
               </a>
@@ -221,7 +218,7 @@ const MainHeader = ({ scrollToElement }) => {
                 version="1.0"
                 xmlns="http://www.w3.org/2000/svg"
                 width="300pt"
-                height="132pt"
+                height="90pt"
                 viewBox="0 0 688.000000 132.000000"
                 preserveAspectRatio="xMidYMid meet"
               >
